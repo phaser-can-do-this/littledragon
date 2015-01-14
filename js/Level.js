86fc20591aef555720c1a386a0b4335bae6d96d9
stormslowly <stormslowly@gmail.com>
2015-01-14 23:18:05 +0800

    dragon can shoot

--- js/Level.js
+++ js/Level.js
@@ -7,38 +7,47 @@ var Level = function(game) {
 Level.prototype = {
   init: function(config) {
     console.log('init ', config);
-    config = config ||{};
+    config = config || {};
     this.level = config.level || 1;
     this.score = config.score || 0;
   },
 
-  preload: function() {
-  },
+  preload: function() {},
 
   create: function() {
     this.add.image(0, 0, 'level' + this.level);
-    this.dragon = this.add.sprite(200, 145, 'dragon');
+    this.dragon = this.add.sprite(200, 160, 'dragon');
+    this.dragon.anchor.setTo(0.5,0.5);
     this.dragon.animations.add('idle', [
-        'dragon_06.gif',
-        'dragon_07.gif',
+      'dragon_06.gif',
+      'dragon_07.gif',
     ], 8, true, false);
-    this.dragon.animations.add('fire', [
-            'dragon_06.gif',
-            'dragon_07.gif',
-            'dragon_08.gif',
-            'dragon_17.gif',
-            'dragon_18.gif',
-            'dragon_19.gif',
-        ], 8,true, false);
-
+    this.dragonFire = this.dragon.animations.add('fire', [
+      'dragon_06.gif',
+      'dragon_07.gif',
+      'dragon_08.gif',
+      'dragon_17.gif',
+      'dragon_18.gif',
+      'dragon_19.gif',
+    ], 8, true, false);
     this.dragon.animations.play('fire');
+    console.log(this.dragon);
+    this.dragonFire.onLoop.add(function() {
 
-    this.bomb = this.add.sprite(100, 140, 'bomb');
-    this.physics.enable(this.bomb, Phaser.Physics.ARCADE);
-    this.bomb.body.velocity.y=200;
-    this.bomb.scale.setTo(0.2);
-    this.add.tween(this.bomb.scale).to( { x: 1, y: 1 }, 800, Phaser.Easing.Linear.None, true);
+      this.bomb = this.add.sprite(this.dragon.x,
+        this.dragon.y, 'bomb');
+      this.bomb.anchor.setTo(0.5);
+      this.physics.enable(this.bomb, Phaser.Physics.ARCADE);
+      this.bomb.body.velocity.y = 200;
+      this.bomb.scale.setTo(0.2);
+      this.add.tween(this.bomb.scale).to({
+        x: 1,
+        y: 1
+      }, 800, Phaser.Easing.Linear.None, true);
+      this.bomb.checkWorldBounds = true;
+      this.bomb.outOfBoundsKill = true;
 
+    }, this);
 
 
 
